2ec78948a356d8970829af7cbe6125227fc02589
azzamzaid10@gmail.com <azzamzaid10@gmail.com>
2023-12-08 09:27:46 +0700

    ubah migration user, dan menambahkan migration lainnya

---
+++ app/Http/Controllers/ToolkitController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Toolkit;
+use Illuminate\Http\Request;
+
+class ToolkitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Toolkit  $toolkit
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Toolkit $toolkit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Toolkit  $toolkit
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Toolkit $toolkit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Toolkit  $toolkit
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Toolkit $toolkit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Toolkit  $toolkit
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Toolkit $toolkit)
+    {
+        //
+    }
+}
--- database/migrations/2023_12_08_022045_create_artikels_table.php
+++ database/migrations/2023_12_08_022045_create_artikels_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('artikels', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('image')->nullable();
+            $table->string('judul');
+            $table->text('deskripsi');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('artikels');
     }
 };
