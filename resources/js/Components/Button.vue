8f091a767f5e8870ffbc46b9d00e2861a33d4beb
Alfina Balqis Nurzaharani <alfinabalqis28@gmail.com>
2023-12-13 08:47:28 +0700

    feat: add hero section'

--- resources/js/Components/Button.vue
+++ resources/js/Components/Button.vue
@@ -1,7 +1,7 @@
 <template>
     <a 
     :href="link"
-    class=" md:text-white md:bg-customGreen hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-2 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mx-2 text-customGreen bg-white
+    class=" md:text-white md:bg-customGreen hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-2 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 text-customGreen  bg-white
     ">
         <slot/>
     </a>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,11 +1,34 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
 import Navbar from "@/Components/Navbar.vue";
-import Footer from "@/Components/Footer.vue"
+import Footer from "@/Components/Footer.vue";
+import Button from "@/Components/Button.vue"
 </script>
 
 <template>
     <Head title="Bijak Lestari" />
     <Navbar />
-    <Footer />
-</template>
+    <section class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <!-- Hero Section -->
+        <div>
+            <div class="hero bg-gray-800 text-white">
+               <div class="flex justify-between items-end">
+                <div class="px-10 py-16 w-1/2">
+                    <h1 class=" text-white text-4xl font-bold py-4">Belajar lebih mudah <br> Bersama Bijak Lestari</h1>
+                    <p class=" text-white text-sm pt-2 pb-6">🌎 Pelajari Teknik Permaculture yang Revolusioner 🌳 <br>
+                        Menggabungkan Ilmu Pengetahuan dan Kreativitas <br> Untuk Mengubah Kehidupan Anda.</p>
+                    <Button>Belajar Sekarang</Button>
+                </div>
+                <div class="image">
+                    <div class="hero-image w-72">
+                        <img src="/img/ilustrasi/hero.png" alt="Hero Image">
+                    </div>
+                </div>
+               </div>
+            </div>
+
+        </div>
+        <!-- Produk Section -->
+
+    </section>
+<Footer /></template>
