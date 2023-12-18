d2e89b2ef0345094dc958ff5683beac50ed9aaa9
Alfina Balqis Nurzaharani <alfinabalqis28@gmail.com>
2023-12-19 01:15:20 +0700

    style: navbar on hover and active page

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -13,7 +13,8 @@
 
             <ul class="md:flex md:items-center md:px-0 px-6 md:pb-0 pb-10 md:static absolute md:w-auto w-full top-20 duration-700 ease-in bg-customGreen md:bg-white text-white md:text-black"
                 :class="[open ? 'left-0' : 'left-[-100%]']">
-                <li class="md:mx-4 md:my-0 my-6" v-for="link in links" :key="link.name">
+                <li class="md:mx-4 md:my-0 my-6" v-for="link in links" :key="link.name"
+                    :class="{ 'font-bold underline': isLinkActive(link.link) }">
                     <a :href="link.link" class="text-md hover:text-green-700">{{ link.name }}</a>
                 </li>
 
@@ -32,7 +33,7 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import Button from "./Button.vue";
 export default {
     components: {
@@ -52,7 +53,36 @@ export default {
             open.value = !open.value
         }
 
-        return { links, open, menuOpen };
+        // Function to check if a link is active
+        function isLinkActive(link) {
+            const currentPath = window.location.pathname;
+
+            if (
+                (link === "/artikel" && currentPath.startsWith("/artikel/detail/")) ||
+                (link === "/toolskit" && currentPath.startsWith("/toolskit/detail/"))
+            ) {
+                return true;
+            }
+
+            return currentPath.endsWith(link);
+        }
+
+        // Add event listener to update styles on route change
+        onMounted(() => {
+            window.addEventListener("popstate", () => {
+                // Update styles on route change
+                links.forEach((link) => {
+                    link.isActive = isLinkActive(link.link);
+                });
+            });
+
+            // Initialize link state on component mount
+            links.forEach((link) => {
+                link.isActive = isLinkActive(link.link);
+            });
+        });
+
+        return { links, open, menuOpen, isLinkActive };
     }
 }
 </script>
